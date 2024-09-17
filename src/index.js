c4cd26cb566ece9014c123841810b5c48fa69084
Vladislav Melnikov <81232295+mindoken@users.noreply.github.com>
2024-09-17 08:54:17 +0300

    lab0

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-alert("Let's integrate!")
+alert("Let's integrate!");
+const a = parseFloat(prompt('a'));
+const b = parseFloat(prompt('b'));
+const step = parseFloat(prompt('step'));
+console.log(step);
+let sum = 0;
+    for (let i=a+step; i < b; i+=step){
+        y = i*i-i+1;
+        console.log(y*step);
+        sum=sum+y*step;
+        
+    }
+    console.log(sum);
+    document.write(sum);
